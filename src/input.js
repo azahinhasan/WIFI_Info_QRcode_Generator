c0c75be6af8f9a5a731c9c50c8c37e3bbafac776
A H Zahin <azahinhasan@gmail.com>
2021-02-04 21:45:28 +0600

    import classes solved

--- src/input.js
+++ src/input.js
@@ -12,19 +12,46 @@ class Input extends Component{ //class based compornent....
         return(
     
         <div> 
-               
-            <input type="text" onChange={this.props.onSSIDchange}/>
-            <br></br>
-            <input type="text" onChange={this.props.onPASSWORDchange}/>
-            <br></br>
-            <select name="" id="" onChange={this.props.onENCRYPTIONchange}>
-            <option value="WPA">WPA/WPA2</option>
-            <option value="WEP">WEP</option>
-            <option value="nopass">None</option>
-            </select>
             
-            <br></br>
-            <input type="checkbox" onClick={this.props.onHIDDENchange}/>Hidden
+            <table>
+                <tr>
+                    <td>
+                    SSID:
+                    </td>
+                    <td>
+                    <input type="text" onChange={this.props.onSSIDchange}/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    PASSWORD:
+                    </td>
+                    <td>
+                    <input type="text" onChange={this.props.onPASSWORDchange}/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    ENCRYPTION:
+                    </td>
+                    <td>
+                    <select name="" id="" onChange={this.props.onENCRYPTIONchange}>
+                        <option value="WPA">WPA/WPA2</option>
+                        <option value="WEP">WEP</option>
+                        <option value="nopass">None</option>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                   
+                    </td>
+                    <td>
+                    <input type="checkbox" onClick={this.props.onHIDDENchange}/>Hidden
+                    </td>
+                </tr>
+            </table>
+            
        
             
         </div>
